b81fd3116aa8aa2dfe4e0aa6697ec986723e2211
rbryanben <rbryanben@gmail.com>
2021-05-29 15:33:54 +0200

    removing animation

--- backend/static/js/console.js
+++ backend/static/js/console.js
@@ -1,23 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     setTimeout(() => {
-        document.querySelector(".loading-modal-body-subtext").innerHTML = "oops! still building, mean-while  lets consume your CPU"
-        window.addEventListener("mousemove", e => {
-            var modal = document.querySelector(".loading-modal-body")
-            mouseXPercent = e.pageX / window.innerWidth
-            mouseYPercentage = e.pageY / window.innerHeight
-
-            //adjustments from the center
-
-
-            skewX = mouseXPercent * 45
-            skewY = mouseYPercentage * 45
-
-            modal.style.transform = "rotateY(" + skewX + "deg)"
-            modal.style.transform = "rotateX(" + skewY + "deg)"
-            modal.style.transform = "rotateZ(" + -skewY + "deg)"
-
-        })
+        document.querySelector(".loading-modal-body-subtext").innerHTML = "oops! we are still bulding"
     }, 6000)
 
     document.getElementById("loading-image").onload = function() {
@@ -25,9 +9,4 @@ window.addEventListener('DOMContentLoaded', function() {
         modal.classList.add("show")
     }
 
-
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * 5);
-    }
 })
