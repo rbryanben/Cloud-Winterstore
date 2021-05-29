52e5ba70115500d44e007c7795399d176897e433
rbryanben <rbryanben@gmail.com>
2021-05-29 14:01:43 +0200

    console loading animation update

--- backend/static/js/console.js
+++ backend/static/js/console.js
@@ -2,30 +2,31 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setTimeout(() => {
         document.querySelector(".loading-modal-body-subtext").innerHTML = "oops! still building, mean-while  lets consume your CPU"
-    }, 4000)
+        window.addEventListener("mousemove", e => {
+            var modal = document.querySelector(".loading-modal-body")
+            mouseXPercent = e.pageX / window.innerWidth
+            mouseYPercentage = e.pageY / window.innerHeight
+
+            //adjustments from the center
+
+
+            skewX = mouseXPercent * 45
+            skewY = mouseYPercentage * 45
+            console.warn(skewX)
+
+            modal.style.transform = "rotateY(" + skewX + "deg)"
+            modal.style.transform = "rotateX(" + skewY + "deg)"
+            modal.style.transform = "rotateZ(" + -skewY + "deg)"
+
+        })
+    }, 6000)
 
     document.getElementById("loading-image").onload = function() {
         var modal = document.querySelector(".loading-modal-body")
         modal.classList.add("show")
     }
 
-    window.addEventListener("mousemove", e => {
-        var modal = document.querySelector(".loading-modal-body")
-        mouseXPercent = e.pageX / window.innerWidth
-        mouseYPercentage = e.pageY / window.innerHeight
 
-        //adjustments from the center
-
-
-        skewX = mouseXPercent * 45
-        skewY = mouseYPercentage * 45
-        console.warn(skewX)
-
-        modal.style.transform = "rotateY(" + skewX + "deg)"
-        modal.style.transform = "rotateX(" + skewY + "deg)"
-        modal.style.transform = "rotateZ(" + -skewY + "deg)"
-
-    })
 
     function getRandomInt(max) {
         return Math.floor(Math.random() * 5);
