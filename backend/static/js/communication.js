e2bda3e8910b881357ece3778244f9bf103d0c60
rbryanben <rbryanben@gmail.com>
2021-06-09 00:19:55 +0200

    download tracker

--- backend/static/js/communication.js
+++ backend/static/js/communication.js
@@ -18,11 +18,35 @@ function postToServer(url, data, callback) {
 
 
  //function downloads a file
+ let currentlyDownloading = false
+ let xhttp = new XMLHttpRequest();
+
  function downloadFile(data,filename){
-    xhttp = new XMLHttpRequest();
+    if (currentlyDownloading){
+        ShowWarning("Multiple Download Unsupported")
+        return
+    }
+    showDownloads()
+    currentlyDownloading = true
+    //add object to download list
+    var downloadList = document.querySelector(".downloads-list")
+    var elementFrame = `<div id="${filename}download" class="download-item">
+    <div class="download-item-header"> <span onclick="cancelCurrentDownload()" class="download-cancel">Cancel</span><i class="fas fa-file-download"></i><span style="margin-left: 5px;">${filename}</span></div>
+    <div class="download-bar"><div id="${filename}progress" class="download-progress"></div></div>
+    </div>`
+
+    downloadList.innerHTML = elementFrame ;
+    var downloadObject = document.getElementById(filename+"download")
+    var progressObject = document.getElementById(filename+"progress")
+   
     xhttp.onreadystatechange = function() {
         var a;
         if (xhttp.readyState === 4 && xhttp.status === 200) {
+            currentlyDownloading = false
+            downloadList.innerHTML = ""
+            setTimeout(()=>{
+                hideDownloads()
+            },1000)
             // Trick for making downloadable link
             a = document.createElement('a');
             a.href = window.URL.createObjectURL(xhttp.response);
@@ -31,14 +55,29 @@ function postToServer(url, data, callback) {
             a.style.display = 'none';
             document.body.appendChild(a);
             a.click();
-            ShowSuccess("Download Started")
         }
        
     };
+
+    //get precentage 
+    xhttp.addEventListener("progress",(e)=>{
+        var precentage = (e.loaded / e.total) * 100 ;
+        progressObject.style.width = precentage + "%"
+    })
+
     // Post data to URL which handles post request
     xhttp.open("POST", "/console/get-file");
     xhttp.setRequestHeader("Content-Type", "application/json");
     // You should set responseType as blob for binary responses
     xhttp.responseType = 'blob';
     xhttp.send(JSON.stringify(data));
+}
+
+function cancelCurrentDownload(){
+    xhttp.abort();
+    currentlyDownloading = false
+    document.querySelector(".downloads-list").innerHTML = ""
+    setTimeout(()=>{
+        hideDownloads()
+    },1000)
 }
