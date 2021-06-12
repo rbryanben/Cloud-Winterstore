7b67c930ad49022614eb162b37540c584efabafe
rbryanben <rbryanben@gmail.com>
2021-06-12 17:58:24 +0200

    set upload and download modal to apper when shift if pressed

--- backend/static/js/console.js
+++ backend/static/js/console.js
@@ -21,16 +21,45 @@ function disableScrolling() {
     window.onscroll = function() { window.scrollTo(x, y); };
 }
 
+
+let key1 = false
+let key2 = false
+let optionKey = 0
+
 document.addEventListener('keydown',(event)=>{
-    if (event.keyCode == 85){
-        hideDownloads()
-        showUploads()
+    if (event.keyCode == 16){
+        key1 = true
     }
-    else if (event.keyCode == 68){
+    if (event.keyCode == 68){
+        key2 = true
+        optionKey = 68
+    }
+    if (event.keyCode == 85){
+        key2 = true
+        optionKey = 85
+    }
+
+    if (key2 && key1 && optionKey == 68){
         hideUploads()
         showDownloads()
     }
+    if (key2 && key1 && optionKey == 85){
+        hideDownloads()
+        showUploads()
+    }
 })
+
+document.addEventListener('keyup',(event)=>{
+    if (event.keyCode == 16){
+        key1 = false
+        optionKey = 0
+    }
+    if (event.keyCode == 68){
+        key2 = false
+        optionKey = 0
+    }
+})
+
 
 
 
