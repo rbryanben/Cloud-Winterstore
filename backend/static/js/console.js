b7493f1f2e4c94db16f4717b94d0917956059d53
rbryanben <rbryanben@gmail.com>
2021-06-21 21:36:04 +0200

    index object id selection and padding adjustments for folder categorizer items

--- backend/static/js/console.js
+++ backend/static/js/console.js
@@ -72,6 +72,31 @@ document.addEventListener('keyup',(event)=>{
 })
 
 
+//for document 
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      ShowSuccess("Copied File ID")
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
+
 
 
 
