1fc07f93d69b90e8327304ac059eb0a50c6cc6d2
rbryanben <rbryanben@gmail.com>
2021-06-29 12:47:04 +0200

    Download Stats: backend model

--- backend/Console/models.py
+++ backend/Console/models.py
@@ -1,3 +1,11 @@
+from RestAPI.views import download
 from django.db import models
+from SharedApp.models import IndexObject
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class FileDownloadInstance(models.Model):
+    file = models.ForeignKey(IndexObject,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    downloaded = models.DateTimeField(auto_now=True)
