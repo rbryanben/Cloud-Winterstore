005938565e514efee3d28b77f5d1886ef58a35c8
rbryanben <rbryanben@gmail.com>
2021-07-08 17:10:39 +0200

    Admin Accounts: Search Admin Account

--- backend/static/js/communication.js
+++ backend/static/js/communication.js
@@ -46,6 +46,20 @@ function putToServer(url, data, callback) {
 
 }
 
+function patchToServer(url, data, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(xmlhttp.response);
+        }
+    }
+
+    xmlhttp.open("PATCH", serverURL + url)
+    xmlhttp.setRequestHeader('Content-Type', 'application/json');
+    xmlhttp.send(JSON.stringify(data));
+
+}
+
 function deleteToServer(url, data, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
